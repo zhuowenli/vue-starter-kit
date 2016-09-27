c6980bfedd35a715e9d2384560f2f0086ade3529
zhuowenli <531840344@qq.com>
2016-09-27 11:07:05 +0800

    add bower

--- src/router-config.js
+++ src/router-config.js
@@ -8,9 +8,9 @@
 import Home from './components/Home.vue';
 import About from './components/About.vue';
 import Quote from './components/Quote.vue';
-import page404 from "./components/404.vue";
+import page404 from './components/404.vue';
 
-export function routerConfig(router) {
+export function routerConfig (router) {
     router.map({
         '*': {
             component: page404
@@ -19,13 +19,13 @@ export function routerConfig(router) {
             name: 'home',
             component: Home
         },
-        '/about':{
+        '/about': {
             name: 'about',
             component: About
         },
-        '/quote':{
+        '/quote': {
             name: 'quote',
             component: Quote
         }
-    })
+    });
 }
